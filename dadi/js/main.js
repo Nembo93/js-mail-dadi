8c33417a8e54ab0b6661a27699622070725139ce
nembo93 <francesco.c193@libero.it>
2022-10-25 16:48:06 +0200

    Aggiunge algoritmo per definire condizioni di vincita, perdita e parità

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -1,2 +1,15 @@
 let userScore = Math.round(Math.random() * (5) + 1);
 console.log (userScore);
+
+let iaScore = Math.round(Math.random() * (5) + 1);
+console.log (iaScore);
+
+if (userScore > iaScore){
+    console.log(`hai vinto`);
+}
+else if ( userScore < iaScore){
+    console.log(`hai perso`);
+}
+else {
+    console.log(`parità`);
+}
