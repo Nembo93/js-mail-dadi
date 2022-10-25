e8d1cb8347ae4c88bd22d027dcb56f40b0d74ec6
nembo93 <francesco.c193@libero.it>
2022-10-25 17:17:20 +0200

    Aggiunge funzione per inserire risultati del gioco nell'html.

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -1,15 +1,20 @@
 let userScore = Math.round(Math.random() * (5) + 1);
 console.log (userScore);
+document.getElementById(`user_score`).innerHTML = userScore;
 
 let iaScore = Math.round(Math.random() * (5) + 1);
 console.log (iaScore);
+document.getElementById(`ia_score`).innerHTML = iaScore;
 
 if (userScore > iaScore){
     console.log(`hai vinto`);
+    document.getElementById(`gameResult`).innerHTML = `Hai vinto!`
 }
 else if ( userScore < iaScore){
     console.log(`hai perso`);
+    document.getElementById(`gameResult`).innerHTML = `Hai perso.`
 }
 else {
     console.log(`parità`);
+    document.getElementById(`gameResult`).innerHTML = `Parità.`
 }
